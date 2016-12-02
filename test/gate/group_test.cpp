2faf5f7eb456969739c2922a8a5a339ea6618718
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-12-02 23:25:11 +0900

    テスト追加

---
+++ test/gate/group_test.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+
+#include "gate.hpp"
+#include "gate/library.hpp"
+#include "gate/group.hpp"
+
+using qc::operator"" _bit;
+using qc::operator"" _tbit;
+using qc::operator"" _cbit;
+using qc::X;
+using qc::Group;
+using qc::Gate;
+
+class GroupTest : public ::testing::Test {
+ protected:
+  GroupTest() : group_(Gate::make<X>(0_tbit)) {}
+
+  Group group_;
+};
+
+TEST_F(GroupTest, IsEqual) {
+  auto const x = X(0_tbit);
+  EXPECT_EQ(Group(x), group_);
+  EXPECT_EQ(Group::make(x), group_);
+}
+
+TEST_F(GroupTest, IsNotEqual) {
+  auto const x = X(1_tbit);
+  EXPECT_NE(Group(x), group_);
+  EXPECT_NE(x, group_);
+}
+
+TEST_F(GroupTest, Assignment) {
+  auto group = Group();
+  group = group_;
+  EXPECT_EQ(group, group_);
+}
+
+TEST_F(GroupTest, GetTypeName) {
+  EXPECT_EQ(Group::TYPE_NAME, group_.get_type_name());
+}
+
+TEST_F(GroupTest, IsGroup) {
+  EXPECT_TRUE(group_.is_group());
+}
--- test/gate/x_test.cpp
+++ test/gate/x_test.cpp
@@ -2,14 +2,12 @@
 
 #include "gate.hpp"
 #include "gate/library.hpp"
-#include "gate/group.hpp"
 
 using qc::operator"" _bit;
 using qc::operator"" _tbit;
 using qc::operator"" _cbit;
 using qc::X;
 using qc::Y;
-using qc::Group;
 using qc::Gate;
 
 class XTest : public ::testing::Test {
@@ -29,37 +27,16 @@ TEST_F(XTest, IsNotEqual) {
   EXPECT_NE(Y(0_tbit), x_);
 }
 
+TEST_F(XTest, Assignment) {
+  auto x = X();
+  x = x_;
+  EXPECT_EQ(x, x_);
+}
+
 TEST_F(XTest, GetTypeName) {
   EXPECT_EQ("X", x_.get_type_name());
 }
 
 TEST_F(XTest, IsNotGroup) {
   EXPECT_EQ(false, x_.is_group());
-}
-
-class GroupTest : public ::testing::Test {
- protected:
-  GroupTest() : group_(Gate::make<X>(0_tbit)) {}
-
-  Group group_;
-};
-
-TEST_F(GroupTest, IsEqual) {
-  auto const x = X(0_tbit);
-  EXPECT_EQ(Group(x), group_);
-  EXPECT_EQ(Group::make(x), group_);
-}
-
-TEST_F(GroupTest, IsNotEqual) {
-  auto const x = X(1_tbit);
-  EXPECT_NE(Group(x), group_);
-  EXPECT_NE(x, group_);
-}
-
-TEST_F(GroupTest, GetTypeName) {
-  EXPECT_EQ("Group", group_.get_type_name());
-}
-
-TEST_F(GroupTest, IsGroup) {
-  EXPECT_EQ(true, group_.is_group());
 }
